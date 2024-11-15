0c97a6c492f01c9a32a049926d71facf4c696a1c
Ryan Morrison <RJM239@gmail.com>
2024-11-15 13:29:02 -0800

    Added sprite flip for moving left and right as well as the start of the particle system. More to come. Also bound the camera to the player and zoomed it in.

--- gamejam/player.gd
+++ gamejam/player.gd
@@ -13,6 +13,8 @@ var is_casting_spell = false
 var combo = Vector3(0, 0, 0)
 var colour = Color(0, 0, 0)
 
+var lastDirection = 1
+
 
 #func get_input():
 #	var input_direction = Input.get_vector("left", "right", "up", "down")
@@ -25,8 +27,9 @@ func _physics_process(_delta):
 	
 	##Get the player to move in left / right directions
 	var playerDirection = Input.get_axis("left","right")
-	if playerDirection:
+	if playerDirection != 0:
 		velocity.x = playerDirection * speed
+		directSprite(playerDirection)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed) #Just slows the player so that it's a smooth motion
 	
@@ -41,6 +44,44 @@ func _physics_process(_delta):
 	if Input.is_action_just_released("ui_select"):
 		velocity.y *= 0.5
 	
+	move_and_slide()
+	handle_animation()
+	
+##Function to handle the animation of the player sprite
+func handle_animation():
+	if is_on_floor():
+		if velocity: #For running animation
+			pass
+		else:
+			animationPath.play("idle")
+		pass
+	else: #If jumping animation
+		pass
+	
+	
+	
+	pass
+	
+# Take in an input of direction Left | Right and scale the sprite to flip it
+func directSprite(direction: float) -> void:
+	if direction < 0 and lastDirection != -1:
+		scale.x = -1
+		lastDirection = -1
+	elif direction > 0 and lastDirection != 1:
+		scale.x = -1
+		lastDirection = 1
+	
+	pass
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_text_submit"):
 		if is_casting_spell:
 			var greatestValue = max(combo.x, combo.y, combo.z)
@@ -62,32 +103,4 @@ func _physics_process(_delta):
 			combo.y = combo.y + 1.0
 		if Input.is_action_just_pressed("right"):
 			combo.z = combo.z + 1.0
-	
-	move_and_slide()
-	handle_animation()
-	
-##Function to handle the animation of the player sprite
-func handle_animation():
-	if is_on_floor():
-		if velocity: #For running animation
-			pass
-		else:
-			animationPath.play("idle")
-		pass
-	else: #If jumping animation
-		pass
-	
-	
-	
-	pass
-
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
 	pass
