39216fe5d4ed5efc7b1c55ca1f1a960b2a1b0c72
Kaiya Wangler <lilah1305@gmail.com>
2024-11-27 21:49:44 -0800

    Added more to secret screen, fixed a bug

--- gamejam/pause_menu.gd
+++ gamejam/pause_menu.gd
@@ -6,6 +6,13 @@ func _on_resume_pressed() -> void:
 	main.pauseMenu()
 	sounds.play()
 	pass # Replace with function body.
+	
+	
+	
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+	Engine.time_scale = 1
+	pass # Replace with function body.
 
 
 func _on_quit_pressed() -> void:
