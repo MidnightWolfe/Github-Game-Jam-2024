b64c42107b4f7e0d46f4a15897440f0a2939da18
Kaiya Wangler <lilah1305@gmail.com>
2024-11-30 12:43:48 -0800

    Updated to match main branch, added some things to menus

--- gamejam/level_0.gd
+++ gamejam/level_0.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 	#_add_enemy("Cyan", Vector2(200,-175))
 	#_add_enemy("Magenta", Vector2(800,100))
 	#_add_enemy("Yellow", Vector2(600,-300))
-	_add_boss("Red", Vector2(0, -800))
+	_add_boss("Red", Vector2(5500, -800))
 
 
 func _add_enemy(type: String, pos: Vector2):
